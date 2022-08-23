26f2e9d5da328f21afab0d3911489eb27a81c5ae
RiccardoCadenelli <riccardo.cadenelli@gmail.com>
2022-08-23 18:50:19 +0200

    Aggiornamento Codice: migliorie della scacchjiera. Possibile adesso aggiungere le pedina su una riga alla volta (3 rosse, 3 gialle)

--- src/main/java/com/example/forza_quattro_grid2/ForzaQuattro2Controller.java
+++ src/main/java/com/example/forza_quattro_grid2/ForzaQuattro2Controller.java
@@ -20,6 +20,11 @@ public class ForzaQuattro2Controller {
 
     @FXML
     private GridPane gridTabella = new GridPane();
+    private final static  int RIGHE = 6;
+    private final static int COLONNE = 7;
+    private Giocatore giocatore = new Giocatore();
+    private int counterC0, counterC1, counterC2, counterC3, counterC4,
+                counterC5, counterC6;
 
     @FXML
     private Circle Circle00,Circle01,Circle02,Circle03,Circle04,Circle05,
@@ -32,26 +37,34 @@ public class ForzaQuattro2Controller {
 
     @FXML
     private Rectangle RectC0, RectC1, RectC2, RectC3, RectC4, RectC5, RectC6;
+    ArrayList<Circle> C0 = new ArrayList<>(); ArrayList<Circle> C1 = new ArrayList<>(); ArrayList<Circle> C2 = new ArrayList<>();
+    ArrayList<Circle> C3 = new ArrayList<>(); ArrayList<Circle> C4 = new ArrayList<>(); ArrayList<Circle> C5 = new ArrayList<>();
+    ArrayList<Circle> C6 = new ArrayList<>();
 
     public void inizializzaScacchiera(){
         ArrayList<ArrayList<Circle>> scacchiera = new ArrayList();
-        ArrayList<Circle> C0 = new ArrayList<>();
+        //ArrayList<Circle> C0 = new ArrayList<>();
         C0.add(Circle00); C0.add(Circle01); C0.add(Circle02); C0.add(Circle03); C0.add(Circle04); C0.add(Circle05);
-        ArrayList<Circle> C1 = new ArrayList<>();
+        //ArrayList<Circle> C1 = new ArrayList<>();
         C1.add(Circle10); C1.add(Circle11); C1.add(Circle12); C1.add(Circle13); C1.add(Circle14); C1.add(Circle15);
-        ArrayList<Circle> C2 = new ArrayList<>();
+        //ArrayList<Circle> C2 = new ArrayList<>();
         C2.add(Circle20); C2.add(Circle21); C2.add(Circle22); C2.add(Circle23); C2.add(Circle24); C2.add(Circle25);
-        ArrayList<Circle> C3 = new ArrayList<>();
+        //ArrayList<Circle> C3 = new ArrayList<>();
         C3.add(Circle30); C3.add(Circle31); C3.add(Circle32); C3.add(Circle33); C3.add(Circle34); C3.add(Circle35);
-        ArrayList<Circle> C4 = new ArrayList<>();
+        //ArrayList<Circle> C4 = new ArrayList<>();
         C4.add(Circle40); C4.add(Circle41); C4.add(Circle42); C4.add(Circle43); C4.add(Circle44); C4.add(Circle45);
-        ArrayList<Circle> C5 = new ArrayList<>();
+        //ArrayList<Circle> C5 = new ArrayList<>();
         C5.add(Circle50); C5.add(Circle51); C5.add(Circle52); C5.add(Circle53); C5.add(Circle54); C5.add(Circle55);
-        ArrayList<Circle> C6 = new ArrayList<>();
+        //ArrayList<Circle> C6 = new ArrayList<>();
         C6.add(Circle60); C6.add(Circle61); C6.add(Circle62); C6.add(Circle63); C6.add(Circle64); C6.add(Circle65);
         scacchiera.add(C0); scacchiera.add(C1); scacchiera.add(C2); scacchiera.add(C3); scacchiera.add(C4); scacchiera.add(C5); scacchiera.add(C6);
         ArrayList<Rectangle> RectList = new ArrayList<>();
         RectList.add(RectC0); RectList.add(RectC1); RectList.add(RectC2); RectList.add(RectC3); RectList.add(RectC4); RectList.add(RectC5); RectList.add(RectC6);
+        RectC0.setFill(Paint.valueOf("TRANSPARENT")); RectC1.setFill(Paint.valueOf("TRANSPARENT")); RectC2.setFill(Paint.valueOf("TRANSPARENT"));
+        RectC3.setFill(Paint.valueOf("TRANSPARENT")); RectC4.setFill(Paint.valueOf("TRANSPARENT")); RectC5.setFill(Paint.valueOf("TRANSPARENT"));
+        RectC6.setFill(Paint.valueOf("TRANSPARENT"));
+        counterC0 = RIGHE; counterC1 = RIGHE; counterC2 = RIGHE; counterC3 = RIGHE; counterC4 = RIGHE;
+        counterC5 = RIGHE; counterC6 = RIGHE;
         //System.out.println(scacchiera);
         /*Circle mycircle = new Circle();
         mycircle= scacchiera.get(0).get(0);
@@ -59,6 +72,28 @@ public class ForzaQuattro2Controller {
         System.out.println(scacchiera.get(0).get(0));
         System.out.println(gridTabella.getWidth());
         System.out.println(gridTabella.getHeight());
+    }
+
+    public void updateCounterC0(){
+        counterC0-= 1;
+    }
+    public void updateCounterC1(){
+        counterC1-= 1;
+    }
+    public void updateCounterC2(){
+        counterC2-= 1;
+    }
+    public void updateCounterC3(){
+        counterC3-= 1;
+    }
+    public void updateCounterC4(){
+        counterC4-= 1;
+    }
+    public void updateCounterC5(){
+        counterC5-= 1;
+    }
+    public void updateCounterC6(){
+        counterC6-= 1;
     }
 
     @FXML
@@ -130,88 +165,171 @@ public class ForzaQuattro2Controller {
     }
 
     @FXML
-    protected void onColumnEntered(){
-        //cerchioProva.setFill(Color.RED);
+    protected void onMouseRectC0Clicked(){
+        while (counterC0 >= 0) {
+            if(giocatore.getPlayerMove() == 1) {
+                C0.get(counterC0).setFill(Paint.valueOf("RED"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else if(giocatore.getPlayerMove() == 2) {
+                C0.get(counterC0).setFill(Paint.valueOf("YELLOW"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else
+                giocatore.ChangePlayer();
+            updateCounterC0();
+        }
+        updateCounterC0();
     }
 
     @FXML
-    protected void onColumnExited(){
-        //cerchioProva.setFill(Color.BLUE);
+    protected void onMouseRectC1Clicked(){
+        while(counterC1 >= 0){
+            if(giocatore.getPlayerMove() == 1) {
+                C1.get(counterC1).setFill(Paint.valueOf("RED"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else if(giocatore.getPlayerMove() == 2) {
+                C1.get(counterC1).setFill(Paint.valueOf("YELLOW"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else
+                giocatore.ChangePlayer();
+            updateCounterC1();
+        }
+        updateCounterC1();
     }
 
     @FXML
-    protected void  onMouseColumnClicked(){
+    protected void onMouseRectC2Clicked(){
+        while(counterC2 >= 0){
+            if(giocatore.getPlayerMove() == 1) {
+                C2.get(counterC2).setFill(Paint.valueOf("RED"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else if(giocatore.getPlayerMove() == 2) {
+                C2.get(counterC2).setFill(Paint.valueOf("YELLOW"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else
+                giocatore.ChangePlayer();
+            updateCounterC2();
+        }
+        updateCounterC2();
+    }
 
-        /*int counter = 0;
-        int counter2 = 0;
-        int counter3 = 0;
-        int counter4 = 0;
-        int counter5 = 0;
-        int counter6 = 0;
-        int counter7 = 0;
+    @FXML
+    protected void onMouseRectC3Clicked(){
+        while(counterC3 >= 0){
+            if(giocatore.getPlayerMove() == 1) {
+                C3.get(counterC3).setFill(Paint.valueOf("RED"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else if(giocatore.getPlayerMove() == 2) {
+                C3.get(counterC3).setFill(Paint.valueOf("YELLOW"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else
+                giocatore.ChangePlayer();
+            updateCounterC3();
+        }
+        updateCounterC3();
+    }
 
-        if(firstColumn == null && counter < N_PEDINE){
-            addCircle1C(Circle00);
-            counter++;
+    @FXML
+    protected void onMouseRectC4Clicked(){
+        while(counterC4 >= 0){
+            if(giocatore.getPlayerMove() == 1) {
+                C4.get(counterC4).setFill(Paint.valueOf("RED"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else if(giocatore.getPlayerMove() == 2) {
+                C4.get(counterC4).setFill(Paint.valueOf("YELLOW"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else
+                giocatore.ChangePlayer();
+            updateCounterC4();
         }
-        else{
-            addCircle1C(Circle01); addCircle1C(Circle02); addCircle1C(Circle03);
-            addCircle1C(Circle04); addCircle1C(Circle05);
-            counter++;
+        updateCounterC4();
+    }
+
+    @FXML
+    protected void onMouseRectC5Clicked(){
+        while(counterC5 >= 0){
+            if(giocatore.getPlayerMove() == 1) {
+                C5.get(counterC5).setFill(Paint.valueOf("RED"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else if(giocatore.getPlayerMove() == 2) {
+                C5.get(counterC5).setFill(Paint.valueOf("YELLOW"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else
+                giocatore.ChangePlayer();
+            updateCounterC5();
         }
-        if(secondColumn == null && counter2 < N_PEDINE){
-            addCircle2C(Circle10);
-            counter2++;
+        updateCounterC5();
+    }
+
+    @FXML
+    protected void onMouseRectC6Clicked(){
+        while(counterC6 >= 0){
+            if(giocatore.getPlayerMove() == 1) {
+                C6.get(counterC6).setFill(Paint.valueOf("RED"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else if(giocatore.getPlayerMove() == 2) {
+                C6.get(counterC6).setFill(Paint.valueOf("YELLOW"));
+                giocatore.ChangePlayer();
+                break;
+            }
+            else
+                giocatore.ChangePlayer();
+            updateCounterC6();
         }
-        else {
-            addCircle2C(Circle11); addCircle2C(Circle12); addCircle2C(Circle13);
-            addCircle2C(Circle14);addCircle2C(Circle15);
-            counter2++;
-        }
-        if(thirdColumn == null && counter2 < N_PEDINE){
-            addCircle3C(Circle20);
-            counter3++;
-        }
-        else{
-            addCircle3C(Circle21); addCircle3C(Circle22); addCircle3C(Circle23);
-            addCircle3C(Circle24);addCircle3C(Circle25);
-            counter3++;
-        }
-        if(fourthColumn == null && counter4 < N_PEDINE){
-            addCircle4C(Circle30);
-            counter4++;
-        }
-        else{
-            addCircle4C(Circle31); addCircle4C(Circle32); addCircle4C(Circle33);
-            addCircle4C(Circle34);addCircle4C(Circle35);
-            counter4++;
-        }
-        if(fifthColumn == null && counter5 < N_PEDINE){
-            addCircle5C(Circle40);
-            counter5++;
-        }
-        else{
-            addCircle5C(Circle41); addCircle5C(Circle42); addCircle5C(Circle43);
-            addCircle5C(Circle44);addCircle5C(Circle45);
-            counter5++;
-        }
-        if(sixthColumn == null && counter6 < N_PEDINE){
-            addCircle6C(Circle50);
-            counter6++;
-        }
-        else{
-            addCircle6C(Circle51); addCircle6C(Circle52); addCircle6C(Circle53);
-            addCircle6C(Circle54);addCircle6C(Circle55);
-            counter6++;
-        }
-        if(seventhColumn == null && counter7 < N_PEDINE){
-            addCircle6C(Circle60);
-            counter7++;
-        }
-        else{
-            addCircle7C(Circle61); addCircle7C(Circle62); addCircle7C(Circle63);
-            addCircle7C(Circle64);addCircle7C(Circle65);
-            counter7++;
-        }*/
+        updateCounterC6();
+    }
+
+    @FXML
+    protected void onPlayButtonClick(){
+
+    }
+
+    @FXML
+    protected void onResetButtonClick(){
+        initialize();
+        giocatore.resetPlayerMove();
+        Circle00.setFill(Paint.valueOf("DODGERBLUE")); Circle01.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle02.setFill(Paint.valueOf("DODGERBLUE")); Circle03.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle04.setFill(Paint.valueOf("DODGERBLUE")); Circle05.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle10.setFill(Paint.valueOf("DODGERBLUE")); Circle11.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle12.setFill(Paint.valueOf("DODGERBLUE")); Circle13.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle14.setFill(Paint.valueOf("DODGERBLUE")); Circle15.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle20.setFill(Paint.valueOf("DODGERBLUE")); Circle21.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle22.setFill(Paint.valueOf("DODGERBLUE")); Circle23.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle24.setFill(Paint.valueOf("DODGERBLUE")); Circle25.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle30.setFill(Paint.valueOf("DODGERBLUE")); Circle31.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle32.setFill(Paint.valueOf("DODGERBLUE")); Circle33.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle34.setFill(Paint.valueOf("DODGERBLUE")); Circle35.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle40.setFill(Paint.valueOf("DODGERBLUE")); Circle41.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle42.setFill(Paint.valueOf("DODGERBLUE")); Circle43.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle44.setFill(Paint.valueOf("DODGERBLUE")); Circle45.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle50.setFill(Paint.valueOf("DODGERBLUE")); Circle51.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle52.setFill(Paint.valueOf("DODGERBLUE")); Circle53.setFill(Paint.valueOf("DODGERBLUE"));
+        Circle54.setFill(Paint.valueOf("DODGERBLUE")); Circle55.setFill(Paint.valueOf("DODGERBLUE"));
     }
 }
